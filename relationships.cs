8cf275b8ff54dfbe2437adb3242c004740bc9f8a
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-07-08 01:36:48 -0400

    Create relationships.cs

---
+++ relationships.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class relationships : MonoBehaviour{
+	public int fatherR = 0;
+	public int kingR = 0;
+	public int PrinceR = 0;
+	public int gaurdsR = 0;
+	public int noblesR = 0;
+	
+}
