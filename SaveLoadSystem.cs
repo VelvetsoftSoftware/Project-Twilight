27099e5f140de6c377702fb2a3db82655e026d61
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-07-20 02:41:55 -0400

    Update SaveLoadSystem.cs

--- SaveLoadSystem.cs
+++ SaveLoadSystem.cs
@@ -39,9 +39,23 @@ public class SaveLoadSystem : MonoBehaviour{
 	}
 	
 	public void saveGame(ushort eventID, string filePath) {
-		saveEvents(eventID, filePath);
-		
 		using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write)) {
+			
+			FileInfo fileInfo = new FileInfo(filePath);
+			if (fileInfo.Length == 0) {
+				using (BinaryWriter writer = new BinaryWriter(fs)) {
+					writer.Write(0x56454C534F4654D3UL/*this is VELSOFT♥ for velvetsoft in the commodore PET unshifted PETSCII, this is a header for da game and is 8 bytes*/);
+					writer.Write(new byte[8]);	
+				}
+			} else {
+				fs.Seek(8 + (eventID / 8), SeekOrigin.Begin);
+				int bitIndex = eventID % 8;
+				// Read existing byte, flip bit, write back
+				int b = fs.ReadByte();
+				fs.Seek(-1, SeekOrigin.Current); // Go back one byte
+				fs.WriteByte((byte)(b | (1 << bitIndex)));
+			}
+		
 			fs.Seek(16, SeekOrigin.Begin); // Skip 16 bytes (8 for header/length, 8 for flags)
 			using (BinaryWriter writer = new BinaryWriter(fs)) {
 				writer.Write(stats.funds);
@@ -66,33 +80,18 @@ public class SaveLoadSystem : MonoBehaviour{
 		
 	}
 	
-	public void saveEvents(ushort eventID, string filePath) {
-		FileInfo fileInfo = new FileInfo(filePath);
-		if (fileInfo.Length == 0) {
-			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write)) {
-				using (BinaryWriter writer = new BinaryWriter(fs)) {
-					writer.Write(0x56454C534F4654D3UL/*this is VELSOFT♥ for velvetsoft in the commodore PET unshifted PETSCII, this is a header for da game and is 8 bytes*/);
-					writer.Write(new byte[8]);
-				}
-			}
-		} else {
-			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write)) {
-				fs.Seek(8 + (eventID / 8), SeekOrigin.Begin);
-				int bitIndex = eventID % 8;
-        
-				// Read existing byte, flip bit, write back
-				int b = fs.ReadByte();
-				fs.Seek(-1, SeekOrigin.Current); // Go back one byte
-				fs.WriteByte((byte)(b | (1 << bitIndex)));
-			}
+	public void loadGame(string filePath) {
+		using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+			
+			
+			Bitpacker.loadupacker();
 		}
 	}
 	
-	public void loadGame() {
-		
-	}
-	
-	public void readEvents() {
-		
+	private void readEvents(ushort eventID, string filePath) {
+		using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+			fs.Seek(8 + (eventID / 8), SeekOrigin.Begin);
+
+		}
 	}
 }
