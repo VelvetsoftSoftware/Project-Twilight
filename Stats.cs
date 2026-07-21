70fb3cfa0fd67c883f6c654fcb1e7aacdb7a3289
VelvetsoftSoftware <thebronyp3@gmail.com>
2026-07-20 20:00:57 -0400

    Update Stats.cs

--- Stats.cs
+++ Stats.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class Stats : MonoBehaviour {
     public byte age = 10;
 	public int funds = 244, income = 0;
-	public double height = 4.58, weight  = 89.0, hips = 28.4;
+	public ushort height = 458, weight  = 8900, hips = 2840; // all if pushed left 2, so height would equal 4.58
 	public byte profession = 0;
 	public bool introIsActive = false;
 	
 	[SerializeField]private dates Dates;
 	
-	public float mood = 0.0f;
+	public ushort mood = 0.0f;
 	
 	//Social
 	public ushort elegance = 0, grace = 0, glamor = 0, negotiation = 0;
@@ -21,112 +21,112 @@ public class Stats : MonoBehaviour {
 	//faith
 	public ushort morality = 0, theology = 0, sin = 0, peity = 0;
 	
-	//stat growth multiplier
-	public float social = 1.0f, physical = 1.0f, intelligance = 1.0f, faith = 1.0f, stressgainfactor = 1.0f;
+	//stat growth multiplier shifted left 2 times so i would be 1.00
+	public byte social = 100, physical = 100, intelligance = 100, faith = 100, stressgainfactor = 100;
 	
 	public void introBool() {
 		introIsActive = true;
 	}
 	
 	public void Knight() {
-		physical = 1.5f;
-		social = 1.2f;
-		faith = 0.75f;
+		physical = 150;
+		social = 120;
+		faith = 75;
 		income = 100;
 		profession = 1;
 	}
 	
 	public void Merchant() {
-		social = 1.5f;
-		intelligance = 1.2f;
-		physical = 0.75f;
+		social = 150;
+		intelligance = 120;
+		physical = 75;
 		income = 150;
 		profession = 2;
 	}
 	
 	public void Theologian() {
-		faith = 1.5f;
-		intelligance = 1.2f;
-		physical = 0.5f;
-		social = 0.75f;
+		faith = 150;
+		intelligance = 120;
+		physical = 50;
+		social = 75;
 		reputation = 10;
 		income = 5;
 		profession = 3;
 	}
 	
 	public void Scholar() {
-		intelligance = 1.5f;
-		faith = 1.2f;
-		physical = 0.5f;
+		intelligance = 150;
+		faith = 120;
+		physical = 50;
 		reputation = 2;
 		income = 25;
 		profession = 4;
 	}
 	
 	public void Artist() {
-		social = 0.5f;
-		physical = 0.75f;
+		social = 50;
+		physical = 75;
 		reputation = 10;
 		income = 125;
 		profession = 5;
 	}
 	
 	public void Cartographer() {
-		intelligance = 1.5f;
-		physical = 1.2f;
-		social = 0.75f;
+		intelligance = 150;
+		physical = 120;
+		social = 75;
 		income = 80;
 		profession = 6;
 	}
 	
 	public void Noble() {
-		social = 1.5f;
-		intelligance = 1.2f;
-		physical = 0.75f;
+		social = 150;
+		intelligance = 120;
+		physical = 075;
 		income = 200;
 		profession = 7;
 	}
 	
 	public void Farmer() {
-		physical = 1.5f;
-		faith = 1.2f;
-		intelligance = 0.75f;
-		social = 0.5f;
+		physical = 150;
+		faith = 120;
+		intelligance = 75;
+		social = 50;
 		income = 10;
 		profession = 8;
 	}
 	
 	public void Craftsman() {
-		physical = 1.5f;
-		social = 1.2f;
+		physical = 150;
+		social = 120;
 		income = 90;
 		profession = 9;
 	}
 	
 	private void spring() {
-		physical = physical + 0.2f;
-		social = social + 0.3f;
+		physical = physical + 20;
+		social = social + 30;
 		//const goes up quicker
 		//gives extra points to releationships
 	}
 	
 	private void summer() {
-		physical = physical + 0.3f;
-		intelligance = intelligance - 0.2f;
+		physical = physical + 30;
+		intelligance = intelligance - 20;
 		//preforms better at comp and tournaments
 	}
 	
 	private void autumn() {
-		intelligance = intelligance + 0.2f;
-		social = social - 0.2f;
+		intelligance = intelligance + 20;
+		social = social - 20;
 		//
 	}
 	
 	private void winter() {
-		physical = physical + 0.1f;
-		intelligance = intelligance + 0.1f;
-		faith = faith + 0.3f;
-		social = social - 0.2f;
+		physical = physical + 10;
+		intelligance = intelligance + 10;
+		faith = faith + 30;
+		social = social - 20;
 		//mysticism
 		
 	}
@@ -153,29 +153,29 @@ public class Stats : MonoBehaviour {
 }
 	
 	private void a() {
-		stressgainfactor = stressgainfactor + 0.3f;
-		physical = physical + 0.4f;
+		stressgainfactor = stressgainfactor + 30;
+		physical = physical + 40;
 		//gains extra stress reduction during vacations
 	}
 	
 	private void b() {
-		stressgainfactor = stressgainfactor - 0.2f;
-		intelligance = intelligance - 0.2f;
-		social = social - 0.1f;
+		stressgainfactor = stressgainfactor - 20;
+		intelligance = intelligance - 20;
+		social = social - 10;
 		//art and craftsmenship gos up
 		// gain more stats for art/craftsmenship
 	}
 	
 	private void ab() {
-		intelligance = intelligance + 0.2f;
-		social = social - 0.1f;
-		faith = faith + 0.1f;
+		intelligance = intelligance + 20;
+		social = social - 10;
+		faith = faith + 10;
 		//unlocks unique options to solve delemias
 	}
 	
 	private void o() {
-		social = social + 0.3f;
-		physical = physical + 0.1f;
+		social = social + 30;
+		physical = physical + 10;
 		// natural leader
 	}
 	
